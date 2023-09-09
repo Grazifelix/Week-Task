06c413d19f2701c47055a94b2a3c3087b22e06c0
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-09-09 17:09:11 -0300

    feat: add initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
-import 'app_widget.dart';
+import 'package:weektasks/view/home_page.dart';
+import 'package:weektasks/shared/app_colors.dart';
+
 
 main() {
   runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Week:Task",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: AppColors.yellow),
+      home: HomePage(),
+      initialRoute: HomePage.routeName,
+    );
+  }
 }
