fb785ff74880be01af548120cf935838dd72ec44
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-09-09 17:07:01 -0300

    feat: class global app colors

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color yellow = Color(0xFFFFCD0D);
+  static Color brown = Color(0xFF403403);
+  static Color softYellow = Color(0x93FFCD0D);
+  static Color green = Color(0x47CBCC1F);
+}
